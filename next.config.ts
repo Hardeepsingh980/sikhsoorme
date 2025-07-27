092983d5e974d57b86dbb631398f4e55097bc6ef
Hardeep.io <Hardeep0khalsa122@gmail.com>
2025-07-27 23:44:30 +0530

    fixed build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isGitHubPages = process.env.GITHUB_ACTIONS || false
-const repo = "sikhsoorme" // replace with your repo name
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
