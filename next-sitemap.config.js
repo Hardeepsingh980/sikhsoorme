b644b7f6364e2e72917b1ebaeab632dd4f41d213
Hardeep.io <Hardeep0khalsa122@gmail.com>
2025-09-21 15:07:11 +0530

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://sikhsoorme.com',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    outDir: './out',
+};
+
+export default config;
+  
